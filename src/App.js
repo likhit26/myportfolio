3395c23e754610914151033085ba0ca35c6fe70d
Likhit Varma M <likhitvarma81@gmail.com>
2023-12-24 19:01:26 +0530

    message

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import NavBar  from "./components/NavBar";
+import Home from "./components/Home";
+import Social from "./components/Social";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+   <div>
+
+    <NavBar/>
+    <Home />
+    <About />
+    <Portfolio />
+  
+   <Experience />
+   <Contact />
+   <Social />
+   
+
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+    
+    <div className="max-w-screen-lg p-4 mx-auto flexx flex-col justify-center w-full h-full">
+    <div className="pb-8">
+    <p className="text-4xl font-bold inlie border-b-4 border-gray-500">About</p>
+    </div>
+
+    <p className="text-xl mt-20">
+     lorem50dddddddddddddddddddddd
+     dddddddddddddddddddddddddddddd
+     ddddddddd
+    </p>
+
+    <br/>
+
+    <p className="text-xl">
+
+      lorem50ddddddddddd
+      dddddddddddddddddddd
+      dddddddddddddddddddddddddddddd
+    </p>
+
+    </div>
+    </div>
+  );
+}
+
+export default About
